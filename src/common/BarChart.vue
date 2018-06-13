5382d2d070ae3800349883cd2347d520df9d4f48
Alex <alex.guzey@gmail.com>
2018-06-13 20:09:53 +0300

    add chart vue packages

---
+++ src/common/BarChart.vue
@@ -0,0 +1,20 @@
+<script>
+// import VueCharts from 'vue-chartjs'
+import { Bar, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+  extends: Bar,
+  mixins: [reactiveProp],
+  props: ['options'],
+  methods: {
+    update () {
+      this.$data._chart.update()
+    }
+  },
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
